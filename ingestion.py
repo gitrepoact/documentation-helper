64d939cea769d5b5d5393a1f0e30989fad774d65
“gitrepoact” <gitrepomails@gmail.com>
2025-10-05 13:47:19 +0530

    Removed Async

--- ingestion.py
+++ ingestion.py
@@ -9,7 +9,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_core.documents import Document
 from langchain_openai import OpenAIEmbeddings
-from langchain_pinecone import PineconeVectorStore
+from langchain_pinecone import PineconeVectorStore, Pinecone
 from langchain_tavily import TavilyCrawl, TavilyExtract, TavilyMap
 
 from logger import Colors, log_error, log_header, log_info, log_success, log_warning
@@ -22,16 +22,16 @@ os.environ["SSL_CERT_FILE"] = certifi.where()
 os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
 
 
-embeddings = OpenAIEmbeddings(
-    model="text-embedding-3-small",
-    show_progress_bar=False,
-    chunk_size=50,
-    retry_min_seconds=10,
-)
-chroma = Chroma(persist_directory="chroma_db", embedding_function=embeddings)
-vectorstore = PineconeVectorStore(
-    index_name="langchain-docs-2025", embedding=embeddings
-)
+# embeddings = OpenAIEmbeddings(
+#     model="text-embedding-3-small",
+#     show_progress_bar=False,
+#     chunk_size=50,
+#     retry_min_seconds=10,
+# )
+# chroma = Chroma(persist_directory="chroma_db", embedding_function=embeddings)
+# vectorstore = PineconeVectorStore(
+#     index_name="langchain-docs-2025", embedding=embeddings
+# )
 tavily_extract = TavilyExtract()
 tavily_map = TavilyMap(max_depth=5, max_breadth=20, max_pages=1000)
 
@@ -122,10 +122,18 @@ async def index_documents_async(documents: List[Document], batch_size: int = 50)
         f"📦 VectorStore Indexing: Split into {len(batches)} batches of {batch_size} documents each"
     )
 
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-3-small",
+        show_progress_bar=False,
+        chunk_size=20,
+        retry_min_seconds=10,
+    )
+
+    vectorstore = PineconeVectorStore(index_name="langchain-docs-2025", embedding=embeddings)
     # Process all batches concurrently
-    async def add_batch(batch: List[Document], batch_num: int):
+    def add_batch(batch: List[Document], batch_num: int):
         try:
-            await vectorstore.aadd_documents(batch)
+            vectorstore.add_documents(batch)
             log_success(
                 f"VectorStore Indexing: Successfully added batch {batch_num}/{len(batches)} ({len(batch)} documents)"
             )
@@ -135,8 +143,9 @@ async def index_documents_async(documents: List[Document], batch_size: int = 50)
         return True
 
     # Process batches concurrently
-    tasks = [add_batch(batch, i + 1) for i, batch in enumerate(batches)]
-    results = await asyncio.gather(*tasks, return_exceptions=True)
+    #tasks = [add_batch(batch, i + 1) for i, batch in enumerate(batches)]
+    results = [add_batch(batch, i + 1) for i, batch in enumerate(batches)]
+    #results = await asyncio.gather(*tasks, return_exceptions=True)
 
     # Count successful batches
     successful = sum(1 for result in results if result is True)
@@ -187,7 +196,7 @@ async def main():
     )
 
     # Process documents asynchronously
-    await index_documents_async(splitted_docs, batch_size=500)
+    await index_documents_async(splitted_docs, batch_size=100)
 
     log_header("PIPELINE COMPLETE")
     log_success("🎉 Documentation ingestion pipeline finished successfully!")
